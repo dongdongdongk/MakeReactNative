68da7d0c53c133d64790cd4dc744c8fbcbeba472
dhk939393 <dhk9309@gmail.com>
2024-07-24 23:07:33 +0900

    React

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another Text</Text>
-        <Text>Hello World</Text>
+        <Text style={styles.dummyText}>Another Text</Text>
+        <Text style={styles.dummyText}>Hello World</Text>
         <Button title='TapMe'/>
       </View>
       <StatusBar style="auto" />
@@ -21,4 +21,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText : {
+    margin: 16, 
+    borderWidth: 2, 
+    borderBlockColor:'red', 
+    padding: 16
+
+  }
 });
